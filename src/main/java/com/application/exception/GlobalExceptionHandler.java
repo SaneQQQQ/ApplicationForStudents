c841acb9bea23f1b13b820b9f7640b282ca0e3b6
Oleksandr <sane4q@gamil.com>
2021-12-12 18:25:34 +0200

    Add GlobalExceptionHandler

--- src/main/java/com/application/exception/GlobalExceptionHandler.java
+++ src/main/java/com/application/exception/GlobalExceptionHandler.java
@@ -2,19 +2,34 @@ package com.application.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.persistence.EntityNotFoundException;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(EntityNotFoundException.class)
+    @ExceptionHandler({EntityNotFoundException.class, NoResultException.class})
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ResponseEntity<CustomJsonResponse> entityNotFoundExceptionHandler(EntityNotFoundException exception) {
+    public ResponseEntity<CustomJsonResponse> entityNotFoundExceptionHandler(PersistenceException exception) {
         return new ResponseEntity<>(new CustomJsonResponse(exception.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<CustomJsonResponse> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException exception) {
+        return new ResponseEntity<>(new CustomJsonResponse(exception.getBindingResult().getAllErrors().get(0).getDefaultMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(value= HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<CustomJsonResponse> internalServerErrorHandler(Exception exception) {
+        return new ResponseEntity<>(new CustomJsonResponse(exception.getMessage()),  HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     private static class CustomJsonResponse {
