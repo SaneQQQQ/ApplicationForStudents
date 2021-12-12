d78d2b05eef40314d045bfc16576e8ac28603a9d
Oleksandr <sane4q@gamil.com>
2021-12-12 18:25:48 +0200

    Add new Data in tables

--- src/main/resources/db-structure.sql
+++ src/main/resources/db-structure.sql
@@ -36,29 +36,48 @@ CREATE TABLE students_subjects(
     FOREIGN KEY (subject_id) REFERENCES subjects(id)
 );
 
-INSERT INTO groups(id, title, created_at) VALUES (1, '111', '1996-04-01 12:30');
-INSERT INTO groups(id, title, created_at) VALUES (2, '112', '2002-07-11 18:23');
-INSERT INTO groups(id, title, created_at) VALUES (3, '113', '2014-03-09 21:35');
+INSERT INTO groups(id, title, created_at) VALUES (1, '111', '2020-04-05 12:30');
+INSERT INTO groups(id, title, created_at) VALUES (2, '112', '2020-07-11 13:23');
+INSERT INTO groups(id, title, created_at) VALUES (3, '211', '2019-03-09 21:35');
 
 INSERT INTO subjects(id, title) VALUES (1, 'Math');
 INSERT INTO subjects(id, title) VALUES (2, 'Chemistry');
-INSERT INTO subjects(id, title) VALUES (3, 'GYM');
+INSERT INTO subjects(id, title) VALUES (3, 'Science');
 INSERT INTO subjects(id, title) VALUES (4, 'English');
+INSERT INTO subjects(id, title) VALUES (5, 'Music');
+INSERT INTO subjects(id, title) VALUES (6, 'Geography');
+INSERT INTO subjects(id, title) VALUES (7, 'History');
+INSERT INTO subjects(id, title) VALUES (8, 'Art');
 
 INSERT INTO students(id, first_name, last_name, email, created_at, group_id) VALUES (1, 'Bob', 'Roy', 'bobroy@gmail.com', '2020-04-04 14:50', 1);
-INSERT INTO students(id, first_name, last_name, email, created_at, group_id) VALUES (2, 'Jim', 'Jackson', 'jimjackson@gmail.com', '2020-01-03 10:30', 1);
-INSERT INTO students(id, first_name, last_name, email, created_at, group_id) VALUES (3, 'Tomas', 'Woody', 'tomaswoody@gmail.com', '2020-12-11 18:32', 2);
-INSERT INTO students(id, first_name, last_name, email, created_at, group_id) VALUES (4, 'Tony', 'Montana', 'tonymontana@gmail.com', '2020-01-06 12:20', 3);
+INSERT INTO students(id, first_name, last_name, email, created_at, group_id) VALUES (2, 'Jim', 'Jackson', 'jimjackson@gmail.com', '2020-04-03 10:30', 1);
+INSERT INTO students(id, first_name, last_name, email, created_at, group_id) VALUES (3, 'Tomas', 'Woody', 'tomaswoody@gmail.com', '2020-07-09 18:32', 2);
+INSERT INTO students(id, first_name, last_name, email, created_at, group_id) VALUES (4, 'Tony', 'Montana', 'tonymontana@gmail.com', '2020-07-11 12:20', 2);
+INSERT INTO students(id, first_name, last_name, email, created_at, group_id) VALUES (5, 'Alex', 'Turner', 'alexturner@gmail.com', '2019-03-07 18:23', 3);
+INSERT INTO students(id, first_name, last_name, email, created_at, group_id) VALUES (6, 'Mary', 'Jonson', 'maryjonson@gmail.com', '2019-03-05 19:10', 3);
 
 INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (1, 1, 60);
 INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (1, 2, 80);
 INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (1, 3, 99);
-INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (2, 3, 75);
-INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (2, 4, 34);
-INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (3, 2, 65);
-INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (3, 3, 89);
-INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (4, 1, 45);
-INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (4, 4, 90);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (2, 1, 75);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (2, 2, 34);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (2, 3, 65);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (3, 4, 89);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (3, 5, 45);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (3, 6, 90);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (4, 4, 67);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (4, 5, 98);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (4, 6, 78);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (5, 1, 90);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (5, 2, 96);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (5, 7, 87);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (5, 8, 67);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (6, 1, 58);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (6, 2, 75);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (6, 7, 60);
+INSERT INTO students_subjects(student_id, subject_id, mark) VALUES (6, 8, 89);
+
+
 
 
 
